d88aa91c71a9c5e54c27030e03db7766fab878fc
John <jlev2282@gmail.com>
2018-10-08 00:25:15 -0400

    added borders to jumbotron

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,18 @@
+"use strict";
+
+var info = {
+    name: "John",
+    weight: 245,
+    date: new Date(),
+    last_workout: getLastWorkout(this.date),
+
+}
+
 $("#add-button").on("click", function(){
     event.preventDefault();
-    alert("Exercise Submitted");
+    alert(info.last_workout);
 })
+
+function getLastWorkout(date){
+    return date;
+}
