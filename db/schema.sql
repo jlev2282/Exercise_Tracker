584ad998aa98c8bbc72b950f126f46804596807d
John <jlev2282@gmail.com>
2018-11-10 00:07:27 -0500

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,7 @@
-CREATE TABLE user (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    weight INT(3) NOT NULL,
-    last_workout TIMESTAMP NOT NULL,
-    PRIMARY KEY(id)
-);
+DROP DATABASE IF EXISTS exercise_tracker;
+CREATE DATABASE exercise_tracker;
 
-CREATE TABLE exercises (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    type VARCHAR(20) NOT NULL,
-    PRIMARY KEY(id)
-);
+USE exercise_tracker;
 
 CREATE TABLE stats (
     id INT AUTO_INCREMENT NOT NULL,
@@ -19,6 +9,7 @@ CREATE TABLE stats (
     type VARCHAR(30) NOT NULL,
     cum INT(10),
     dis INT(10),
+    weight INT(3),
     pr INT(10),
     cal INT(10),
     reps INT(10),
@@ -27,7 +18,22 @@ CREATE TABLE stats (
     PRIMARY KEY(id)
 );
 
-INSERT INTO user (name, weight, last_workout)
+CREATE TABLE exercises (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(15) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE user (
+	id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    weight INT(3) NOT NULL,
+    last_workout TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO user (username, weight, last_workout)
 VALUES("John", 245, now());
 
 INSERT INTO exercises (name, type)
